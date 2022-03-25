e7a9a10fac615ded7ea4b39026b50f5d37a848dd
rsanlazaro <rsanlazaro@hotmail.com>
2022-03-25 16:54:20 -0600

    TEST

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -6,6 +6,7 @@ use MVC\Router;
 use Model\Propiedad;
 use Model\Vendedor;
 use Intervention\Image\ImageManagerStatic as Image;
+// use Imagick as Imagick;
 
 class PropiedadController
 {
@@ -33,7 +34,8 @@ class PropiedadController
  
             if ($_FILES['propiedad']['tmp_name']['imagen']) {
                 // $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800, 600);
-                $image = new Imagick($_FILES['propiedad']['tmp_name']['imagen'])->scaleImage(800, 600);
+                $image = new Imagick($_FILES['propiedad']['tmp_name']['imagen']);
+                $image->scaleImage(800, 600);
                 
                 $propiedad->setImagen($nombreImagen);
             }
@@ -67,7 +69,8 @@ class PropiedadController
             $nombreImagen = md5(uniqid(rand(), true)) . ".jpg";
             if ($_FILES['propiedad']['tmp_name']['imagen']) {
                 // $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800, 600);
-                $image = new Imagick($_FILES['propiedad']['tmp_name']['imagen'])->scaleImage(800, 600);
+                $image = new Imagick($_FILES['propiedad']['tmp_name']['imagen']);
+                $image->scaleImage(800, 600);
                 $propiedad->setImagen($nombreImagen);
             }
             $errores = $propiedad->validar();
