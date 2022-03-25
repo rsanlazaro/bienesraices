2a4fcbfaa6544fe2ac47474d7245487e9b32db8e
rsanlazaro <rsanlazaro@hotmail.com>
2022-03-25 16:30:18 -0600

    TEST

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -5,7 +5,7 @@ namespace Controllers;
 use MVC\Router;
 use Model\Propiedad;
 use Model\Vendedor;
-use Intervention\Image\ImageManagerStatic as Image;
+// use Intervention\Image\ImageManagerStatic as Image;
 
 class PropiedadController
 {
@@ -30,37 +30,24 @@ class PropiedadController
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $propiedad = new Propiedad($_POST['propiedad']);
             $nombreImagen = md5(uniqid(rand(), true)) . ".jpg";
-            // echo "<pre>";
-            // var_dump($_FILES);
-            // echo "</pre>";
-            // exit;
+ 
             if ($_FILES['propiedad']['tmp_name']['imagen']) {
-                echo "<pre>";
-                var_dump($_FILES['propiedad']['tmp_name']['imagen']);
-                echo "</pre>";
-                $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800, 600);
-                echo "<pre>";
-                echo "IMAGE:";
-                var_dump($image);
-                echo "</pre>";
+                // $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800, 600);
+                $image = new Imagick($_FILES['propiedad']['tmp_name']['imagen'])->scaleImage(800, 600);
+                
                 $propiedad->setImagen($nombreImagen);
-                echo "<pre>";
-                echo "PROPIEDAD";
-                var_dump($propiedad);
-                echo "</pre>";
-                exit;
             }
             $errores = $propiedad->validar();
             if (empty($errores)) {
-                // if (!is_dir(CARPETA_IMAGENES)) {
-                //     mkdir(CARPETA_IMAGENES);
-                // }
+                if (!is_dir(CARPETA_IMAGENES)) {
+                    mkdir(CARPETA_IMAGENES);
+                }
                 // $image->save(CARPETA_IMAGENES . $nombreImagen);
+                $image->writeImage(CARPETA_IMAGENES . $nombreImagen);
                 $propiedad->guardar();
             }
         }
         $view = $_SERVER['REDIRECT_URL'];
-        // 'propiedades/crear'
         $router->render($view, [
             'propiedad' => $propiedad,
             'vendedores' => $vendedores,
@@ -79,7 +66,8 @@ class PropiedadController
             $propiedad->sincronizar($args);
             $nombreImagen = md5(uniqid(rand(), true)) . ".jpg";
             if ($_FILES['propiedad']['tmp_name']['imagen']) {
-                $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800, 600);
+                // $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800, 600);
+                $image = new Imagick($_FILES['propiedad']['tmp_name']['imagen'])->scaleImage(800, 600);
                 $propiedad->setImagen($nombreImagen);
             }
             $errores = $propiedad->validar();
@@ -88,7 +76,8 @@ class PropiedadController
                     mkdir(CARPETA_IMAGENES);
                 }
                 if ($_FILES['propiedad']['tmp_name']['imagen']) {
-                    $image->save(CARPETA_IMAGENES . $nombreImagen);
+                    $image->writeImage(CARPETA_IMAGENES . $nombreImagen);
+                    // $image->save(CARPETA_IMAGENES . $nombreImagen);
                 }
                 $propiedad->guardar();
             }
