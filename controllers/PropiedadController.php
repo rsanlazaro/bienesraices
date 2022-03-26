0a9bbc259826a0f13190b09471ef51d2ed313d27
rsanlazaro <rsanlazaro@hotmail.com>
2022-03-25 20:16:53 -0600

    TEST

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -6,7 +6,6 @@ use MVC\Router;
 use Model\Propiedad;
 use Model\Vendedor;
 use Intervention\Image\ImageManagerStatic as Image;
-use Imagick;
 
 class PropiedadController
 {
@@ -33,9 +32,9 @@ class PropiedadController
             $nombreImagen = md5(uniqid(rand(), true)) . ".jpg";
  
             if ($_FILES['propiedad']['tmp_name']['imagen']) {
-                // $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800, 600);
-                $image = new Imagick($_FILES['propiedad']['tmp_name']['imagen']);
-                $image->scaleImage(800, 600);
+                $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800, 600);
+                // $image = new Imagick($_FILES['propiedad']['tmp_name']['imagen']);
+                // $image->scaleImage(800, 600);
                 
                 $propiedad->setImagen($nombreImagen);
             }
@@ -44,8 +43,8 @@ class PropiedadController
                 if (!is_dir(CARPETA_IMAGENES)) {
                     mkdir(CARPETA_IMAGENES);
                 }
-                // $image->save(CARPETA_IMAGENES . $nombreImagen);
-                $image->writeImage(CARPETA_IMAGENES . $nombreImagen);
+                $image->save(CARPETA_IMAGENES . $nombreImagen);
+                // $image->writeImage(CARPETA_IMAGENES . $nombreImagen);
                 $propiedad->guardar();
             }
         }
@@ -68,9 +67,9 @@ class PropiedadController
             $propiedad->sincronizar($args);
             $nombreImagen = md5(uniqid(rand(), true)) . ".jpg";
             if ($_FILES['propiedad']['tmp_name']['imagen']) {
-                // $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800, 600);
-                $image = new Imagick($_FILES['propiedad']['tmp_name']['imagen']);
-                $image->scaleImage(800, 600);
+                $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800, 600);
+                // $image = new Imagick($_FILES['propiedad']['tmp_name']['imagen']);
+                // $image->scaleImage(800, 600);
                 $propiedad->setImagen($nombreImagen);
             }
             $errores = $propiedad->validar();
@@ -79,8 +78,8 @@ class PropiedadController
                     mkdir(CARPETA_IMAGENES);
                 }
                 if ($_FILES['propiedad']['tmp_name']['imagen']) {
-                    $image->writeImage(CARPETA_IMAGENES . $nombreImagen);
-                    // $image->save(CARPETA_IMAGENES . $nombreImagen);
+                    // $image->writeImage(CARPETA_IMAGENES . $nombreImagen);
+                    $image->save(CARPETA_IMAGENES . $nombreImagen);
                 }
                 $propiedad->guardar();
             }
